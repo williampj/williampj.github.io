c2f837e755179aaefaa81354281a49a57b9eaf68
William Jackson <williampj@gmail.com>
2021-01-11 00:22:13 -0500

    bridgeapi section and most of projects sections completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,16 @@
 /* eslint-disable linebreak-style */
 
 document.addEventListener('DOMContentLoaded', () => {
-  // const smoothScrollToTop = (e) => {
-  //   const offsetTop = document.querySelector(href);
+  const scrollSectionToTopOfWindow = (e) => {
+    e.preventDefault();
 
-  //   scroll({
-  //     top: offsetTop,
-  //     behavior: 'smooth',
-  //   });
-  // };
-
-  document.querySelectorAll('.title').forEach((title) => {
-    title.addEventListener('click', (e) => {
-      e.preventDefault();
-      const section = e.target.parentElement;
-      window.scroll({
-        top: section.offsetTop - 50,
-        left: 0,
-        behavior: 'smooth',
-      });
+    const section = e.target.closest('section');
+    window.scroll({
+      top: section.offsetTop - 50,
+      left: 0,
+      behavior: 'smooth',
     });
-  });
+  };
 
-  // const titles = document.querySelectorAll('.title');
-  // titles.forEach((title) => title.addEventListener('click', (e) => {
-  //   window.scroll({
-  //     e.clientY
-  //     // top: this.offsetTop,
-  //     // behavior: 'smooth',
-  //   });
+  document.querySelectorAll('.title').forEach((title) => title.addEventListener('click', scrollSectionToTopOfWindow));
 });
-// for (const title of titles) {
-//   title.addEventListener('click', smoothScrollToTop);
-// }
-
-// titles.addEventListener('click', () => {
-//   window.scrollTo(this.o, 0);
-//   window.scrollTo(0, 0);
-// });
-
-// window.scrollTo(x - coordinate, y - coordinate);
